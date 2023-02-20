63b78a448553c5fe3052c10537d724599b4550ef
devtuti <tuti92@mail.ru>
2023-02-20 22:59:27 +0300

    1st we create a new component in the component folder. In app.js, we name and import the components. We start those components in welcome.blade.php. We wrote fruits in the component we created

---
+++ resources/js/components/Lesson1.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Fruits</div>
+                    <div class="card-body">
+                        <p>Apple</p>
+                        <p>Banana</p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+
+    }
+</script>
+<style>
+</style>
